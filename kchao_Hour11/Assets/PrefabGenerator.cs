239e1500cc10147ef7ede02c09adcfcc2fed73ae
k <31718966+requiesce@users.noreply.github.com>
2024-02-15 01:02:47 -0800

    ch10

---
+++ kchao_Hour11/Assets/PrefabGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+            //Instantiate(prefab, transform.position, transform.rotation);
+            Instantiate(prefab, transform.position + new Vector3(transform.position.x + i*2, 0, 0), transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Whenever we hit the B key we will generate a prefab at the
+        // position of the original prefab
+        // Whenever we hit the space key, we will generate a prefab at the
+        // position of the spawn object that this script is attached to
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
